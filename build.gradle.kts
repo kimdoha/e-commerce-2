a01e8843431b4c9929340ff5108325a9e6b25340
doha-kim <doha-kim@nhn-commerce.com>
2025-04-04 09:45:32 +0900

    feat. ERD 다이어그램 작

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,66 +1,83 @@
+val restdocsApiSpecVersion: String by extra { "0.19.4" }
+
 plugins {
-	kotlin("jvm") version "2.1.0"
-	kotlin("kapt") version "2.1.0"
-	kotlin("plugin.spring") version "2.1.0"
-	kotlin("plugin.jpa") version "2.1.0"
-	id("org.springframework.boot") version "3.4.1"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "2.1.0"
+    kotlin("kapt") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
+    kotlin("plugin.jpa") version "2.1.0"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.epages.restdocs-api-spec") version "0.19.4"
 }
 
 fun getGitHash(): String {
-	return providers.exec {
-		commandLine("git", "rev-parse", "--short", "HEAD")
-	}.standardOutput.asText.get().trim()
+    return providers.exec {
+        commandLine("git", "rev-parse", "--short", "HEAD")
+    }.standardOutput.asText.get().trim()
 }
 
 group = "kr.hhplus.be"
 version = getGitHash()
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-		jvmToolchain(17)
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+        jvmToolchain(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
 }
 
 dependencies {
-	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // Spring
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // DB
-	runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.mysql:mysql-connector-j")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:mysql")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // REST Docs
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:${restdocsApiSpecVersion}")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	systemProperty("user.timezone", "UTC")
+    useJUnitPlatform()
+    systemProperty("user.timezone", "UTC")
+}
+
+openapi3 {
+    setServer("https://localhost:8081")
+    title = "E-Commerce API"
+    description = """
+                  | E-Commerce API
+                  """.trimMargin()
+    version = "1.0.0"
+    format = "yaml"
 }
